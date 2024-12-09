ae32e085977cfc03f1a83283df209d40e1656a8b
Aniket <corpjack.aniket@gmail.com>
2024-12-09 16:53:04 +0530

    list rendering

--- reactfirst/src/App.jsx
+++ reactfirst/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   //   return <Component1/>
   // }else{
   //   return <Component2/>
-  // }
+  // } 
 
 
   // const name = "Aniket";
--- reactfirst/src/List/Array Rendering/Student.jsx
+++ reactfirst/src/List/Array Rendering/Student.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
+import Studentdata from './Studentdata'
 
 function Student() {
 
     // const studs = ["Aniket","Rakesh","Divyam","Chandani"];
-    // const numbers =[2,4,6,8];
+    // let studntName = studs.map((stu,index)=><li key={index}>{stu}</li>)
+    // const numbers =[2,4,6,8]; 
     // const result = numbers.map(num=><h1>{num*2}</h1>);
-    //! creating array of objects
+    // ! creating array of objects
     const studs = [
         {
+            id:1,
             name: "Aniket",
             age: 24
         },
         {
+            id:2,
             name: "react",
             age: 2008
         },
         {
+            id:3,
             name: "power",
             age: 2024
         }
     ]
     return (
         <div>
-            {/* {studs.map(std=><h1>{std}</h1>)}       */}
+            {/* {<ul>{studntName}</ul>}       */}
 
             {/* {numbers.map(num=><h1>{num*2}</h1>)} */}
             {/* {result} */}
@@ -30,7 +35,10 @@ function Student() {
             {/* {studs.map(stu=><h1>I am {stu.name} and I am {stu.age}</h1>)} */}
             
              //! passing data to another component
-            {studs.map(stu=><Studentdata stu={stu}/>)}
+             //! without key
+            {/* {studs.map(stu=><Studentdata stu={stu}/>)} */}
+            //! passing data to another component with key 
+            {studs.map(stu=><Studentdata key={stu.id} stu={stu}/>)}
         </div>
     )
 }
