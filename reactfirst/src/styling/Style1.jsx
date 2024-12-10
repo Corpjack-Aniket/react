79be655f4542dd674e7f8fabcae9ca63b1019f61
Aniket <corpjack.aniket@gmail.com>
2024-12-10 13:48:52 +0530

    module import

--- reactfirst/src/styling/Style1.jsx
+++ reactfirst/src/styling/Style1.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import '../styling/Style1.css';
+// import '../styling/Style1.css';
+import Style from '../styling/Style1.module.css';
 
 export default class Style1 extends Component {
   render() {
     return (
       <div>
-        <h1 className='heading1'>This is bg color</h1>
+        {/* <h1 className='heading1'>This is bg color</h1> */}
+        <h1 className={Style.heading1}>This is bg color</h1>
       </div>
     )
   }
--- reactfirst/src/styling/Style2.jsx
+++ reactfirst/src/styling/Style2.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import '../styling/Style2.css';
+// import '../styling/Style2.css';
+import Style from '../styling/Style1.module.css';
 
 export default class Style2 extends Component {
   render() {
     return (
       <div>
-        <h2 className='heading1'>This is second style class and css</h2>
+        {/* <h2 className='heading1'>This is second style class and css</h2> */}
+        <h2 className={Style.heading1}>This is second style class and css</h2>
       </div>
     )
   }
